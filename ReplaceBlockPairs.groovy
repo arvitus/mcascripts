0d43a9fabba4afd497ca51fdc07fe08be1ecbfa4
Royal-Terrapin <132233072+Royal-Terrapin@users.noreply.github.com>
2024-11-11 19:37:58 -0800

    Create ReplaceBlockPairs.groovy

---
+++ ReplaceBlockPairs.groovy
@@ -0,0 +1,25 @@
+import net.querz.mcaselector.io.mca.ChunkData
+import net.querz.nbt.CompoundTag
+
+/**
+ * Defines pairs of blocks to replace.
+ * Replaces the original block in each block pair with its respective destination block.
+ * Note: Block states are not updated, only the block name is changed.
+ *
+ * @type Change NBT (Ctrl + N)
+ * @difficulty INTERMEDIATE
+ */
+static void apply(ChunkData data) {
+    def blockpairs = ['byg:red_rock': 'byg:travertine', 'byg:aspen_log': 'byg:pendorite_block']
+//block replacements are specified here as comma-separated pairs in the format 'source:original_block': 'source:new_block'
+    
+    for (section in data.region()?.data?.getListTag("sections") as List<CompoundTag>) {
+        var blockPalette = section.getCompoundTag("block_states")?.getListTag("palette")
+        if (!blockPalette) continue
+        for (block in blockPalette as List<CompoundTag>) {
+            if (block.getString("Name") in blockpairs) {
+                block.putString("Name", blockpairs[block.getString("Name")])
+            }
+        }
+    }
+}
